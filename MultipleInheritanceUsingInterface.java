52c1883b82e5e211bd1f4ec94a62a35f6a397409
bhaskar singh <146510258+bhaskarsingh696@users.noreply.github.com>
2026-02-01 10:44:17 +0000

    Interface in Java8

---
+++ MultipleInheritanceUsingInterface.java
@@ -0,0 +1,24 @@
+interface A {
+    void showA();
+}
+
+interface B {
+    void showB();
+}
+
+class C implements A, B {
+    public void showA() {
+        System.out.println("Show A");
+    }
+
+    public void showB() {
+        System.out.println("Show B");
+    }
+}
+
+public class MultipleInheritanceUsingInterface {
+
+    public static void main(String[] args) {
+        
+    }
+}
